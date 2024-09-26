61c7f497460ac501f674ec35661c07e74509d467
dzmat <dzmat@yandex.ru>
2024-09-26 21:00:10 +0700

    WIP

--- src/controller/controllerStrangeSort.java
+++ src/controller/controllerStrangeSort.java
@@ -1,9 +1,53 @@
 package controller;
 
-import domain.Thing;
+import domain.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.ToIntFunction;
 
 public final class controllerStrangeSort<T extends Thing> {
+
+//	enum carIntGetters {
+//		WEIGHT("во весу", (Car car) -> car.getMadeYear()),
+//		SPEED("по скорости", (Car car) -> car.getPower());
+//		
+//		private final String displayString;
+//		ToIntFunction<Car> getter;
+//
+//	private carIntGetters(String displayString, ToIntFunction<Car> getter) {
+//
+//        this.getter=getter;
+//        this.displayString = displayString;
+//        
+//    }
+//
+//	}
+
+	enum CollectionType {
+		CAR("Автомобиль"), BOOK("Книга"), VEGETABLE("Корнеплод");
+
+		private final String name;
+		private ArrayList<String> pseudonyms = new ArrayList<>();
+
+		private void setPseudonym(String pseudonym) {
+			this.pseudonyms.add(pseudonym);
+		}
+
+		private CollectionType(String name) {
+			this.name = name;
+			setPseudonym(name);
+			setPseudonym(String.valueOf(this.ordinal() + 1));
+			setPseudonym(this.ordinal() + 1 + ". " + this.getName());
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public ArrayList<String> getPseudonyms() {
+			return pseudonyms;
+		}
+	}
 
 	private List<T> lst;
 
@@ -11,8 +55,15 @@ public final class controllerStrangeSort<T extends Thing> {
 		this.lst = lst;
 	}
 
+	ToIntFunction<T> promptKey() {
+		System.out.println("выберите по какому ключу осуществить странную сортировку:");
+		return car->((Car)car).getMadeYear();
+	}
+
 	public void run() {
 		System.out.println("Работаем с коллекцией " + lst.getClass().getSimpleName());
-
+		ToIntFunction<T> getter = promptKey();
+		Utility.strangeSort(lst, getter);
+		Utility.dumpList(lst);
 	}
 }
