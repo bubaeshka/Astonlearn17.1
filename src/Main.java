60623ac62df0c4eac3bc35f089a1cd8e168d4497
dzmat <dzmat@yandex.ru>
2024-09-27 17:45:31 +0700

    sort interface realization

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,26 @@ import domain.*;
 import domain.Thing;
 
 public class Main {
-	private static void testGeneration() {
-		// testing raw List
-		var list = new readers.RandomReader(new readingStrategy.CarReadingStrategy(), 5).read();
-		Utility.dumpList(list);
-		list = new readers.RandomReader(new readingStrategy.BookReadingStrategy(), 5).read();
-		Utility.dumpList(list);
-		list = new readers.RandomReader(new readingStrategy.VegetableReadingStrategy(), 5).read();
-		Utility.dumpList(list);
-	}
 
 	static <T extends Thing> void prompt(T vart) {
 		String type = vart.getClass().getSimpleName();
 		System.out.println(type);
 	}
 
-	static <T extends Thing> void testStrange() {
+	static  void testStrange() {
 		var list = new readers.RandomReader<Car>(new readingstrategy.CarReadingStrategy(), 5).read();
 		utility.Utility.dumpList(list);
 		var ctrl = new controller.ControllerStrangeSort<Car>(list);
 		ctrl.run();
 	}
+	static void testSort() {
+		var list = new readers.RandomReader<Car>(new readingstrategy.CarReadingStrategy(), 5).read();
+		utility.Utility.dumpList(list);
+		var ctrl = new controller.SortingCtrl<Car>(list);
+		ctrl.run();
+	}
 
 	public static void main(String[] args) {
-		testStrange();
+		testSort();
 	}
 }
